a94728d7c70cad9c07413fa151f820f7216f533d
jana <janardhanyadav206@gmail.com>
2024-05-19 10:50:37 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,27 @@
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "hello world from react"
+// );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+// <div id="parent">
+//      <div id="child">
+//              <h1>hello world</h1>
+//
+//  </div>
+// </div>
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "hello world")
+  )
+);
+
+root.render(parent);
