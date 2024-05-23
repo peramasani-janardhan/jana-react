dcd4f83c1136c9b946e4c5abaf880c975177315c
jana <janardhanyadav206@gmail.com>
2024-05-23 21:12:00 +0530

    changes

--- dist/index.6bd02f5a.js
+++ dist/index.6bd02f5a.js
@@ -3,29 +3,19 @@
 //   { id: "heading" },
 //   "hello world from react"
 // );
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 // root.render(heading);
-
 // <div id="parent">
 //      <div id="child">
 //              <h1>hello world</h1>
 //
 //  </div>
 // </div>
-
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "hello world")
-  )
-);
-
+const parent = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {
+    id: "child"
+}, React.createElement("h1", {}, "hello world")));
 root.render(parent);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
