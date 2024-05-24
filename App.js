739394dbd102cb2891d92d4fc69be6034ee3c8de
jana <janardhanyadav206@gmail.com>
2024-05-24 06:54:08 +0530

    parcel changes

--- App.js
+++ App.js
@@ -1,31 +1,8 @@
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "hello world from react"
-// );
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading);
-
-// <div id="parent">
-//      <div id="child">
-//              <h1>hello world</h1>
-//
-//  </div>
-// </div>
-
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "hello world")
-  )
-);
-
-root.render(parent);
+const App = () => {
+  return (
+    <>
+      <h1>hello world</h1>
+    </>
+  );
+};
+export default App;
